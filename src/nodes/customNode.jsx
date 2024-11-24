e994ef9c359cf28ae0c9a6780641ea73fdcfb195
Pruthvik Matrawadiya <pmatrawadiya@Pruthviks-MacBook-Air.local>
2024-11-24 12:56:17 +0530

    feat: added custom node

---
+++ src/nodes/customNode.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import BaseNode from "../components/BaseNode/BaseNode";
+import InputField from "../components/InputField/InputField";
+import { MdOutlineInput } from "react-icons/md";
+import { Position } from "reactflow";
+
+export function CustomNode({ id, data, selected }) {
+  const [name, setName] = useState("");
+  return (
+    <>
+      <BaseNode selected={selected}>
+        <BaseNode.TitleBar data={data} Icon={MdOutlineInput}>
+          Text
+        </BaseNode.TitleBar>
+        <InputField
+          id={id}
+          label={"Name"}
+          value={name}
+          onChange={setName}
+        ></InputField>
+
+        <BaseNode.Handle
+          type="source"
+          position={Position.Right}
+          id={`${id}-prompt`}
+        />
+      </BaseNode>
+    </>
+  );
+}
