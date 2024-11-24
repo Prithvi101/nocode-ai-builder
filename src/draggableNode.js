473fc24f4882af1e24013f45e4d7d8ea6d7e09f6
Pruthvik Matrawadiya <pmatrawadiya@Pruthviks-MacBook-Air.local>
2024-11-24 12:52:17 +0530

    chore: updated styling for ui

--- src/draggableNode.js
+++ src/draggableNode.js
@@ -1,33 +1,30 @@
-// draggableNode.js
-
-export const DraggableNode = ({ type, label }) => {
-    const onDragStart = (event, nodeType) => {
-      const appData = { nodeType }
-      event.target.style.cursor = 'grabbing';
-      event.dataTransfer.setData('application/reactflow', JSON.stringify(appData));
-      event.dataTransfer.effectAllowed = 'move';
-    };
-  
-    return (
-      <div
-        className={type}
-        onDragStart={(event) => onDragStart(event, type)}
-        onDragEnd={(event) => (event.target.style.cursor = 'grab')}
-        style={{ 
-          cursor: 'grab', 
-          minWidth: '80px', 
-          height: '60px',
-          display: 'flex', 
-          alignItems: 'center', 
-          borderRadius: '8px',
-          backgroundColor: '#1C2536',
-          justifyContent: 'center', 
-          flexDirection: 'column'
-        }} 
-        draggable
-      >
-          <span style={{ color: '#fff' }}>{label}</span>
-      </div>
+export const DraggableNode = ({ type, label, Icon }) => {
+  const onDragStart = (event, nodeType) => {
+    const appData = { nodeType };
+    event.target.style.cursor = "grabbing";
+    event.dataTransfer.setData(
+      "application/reactflow",
+      JSON.stringify(appData)
     );
+    event.dataTransfer.effectAllowed = "move";
   };
-  
+
+  return (
+    <div
+      onDragStart={(event) => onDragStart(event, type)}
+      onDragEnd={(event) => (event.target.style.cursor = "grab")}
+      className={`border border-gray-300 hover:border-purple-500 group hover:text-purple-500 
+                  bg-white shadow-sm hover:shadow-md 
+                  cursor-grab transition-shadow ease-in-out duration-300 
+                  p-4 text-gray-700 font-semibold items-center 
+                  gap-2 rounded-xl min-w-[5rem] flex flex-col 
+                  aspect-square ${type}`}
+      draggable
+    >
+      {Icon && (
+        <Icon className="text-3xl text-gray-400 group-hover:text-purple-500 " />
+      )}
+      <span className="text-center">{label}</span>
+    </div>
+  );
+};
--- src/toolbar.js
+++ src/toolbar.js
@@ -1,17 +1,21 @@
 // toolbar.js
 
-import { DraggableNode } from './draggableNode';
+import { DraggableNode } from "./draggableNode";
+import { LuFileOutput, LuFileInput, LuText, LuServer } from "react-icons/lu";
+import { SubmitButton } from "./submit";
 
 export const PipelineToolbar = () => {
-
-    return (
-        <div style={{ padding: '10px' }}>
-            <div style={{ marginTop: '20px', display: 'flex', flexWrap: 'wrap', gap: '10px' }}>
-                <DraggableNode type='customInput' label='Input' />
-                <DraggableNode type='llm' label='LLM' />
-                <DraggableNode type='customOutput' label='Output' />
-                <DraggableNode type='text' label='Text' />
-            </div>
-        </div>
-    );
+  return (
+    <div className="px-10  py-4 flex justify-between items-center">
+      <div className="flex gap-4">
+        <DraggableNode type="customInput" label="Input" Icon={LuFileInput} />
+        <DraggableNode type="llm" label="LLM" Icon={LuServer} />
+        <DraggableNode type="customOutput" label="Output" Icon={LuFileOutput} />
+        <DraggableNode type="text" label="Text" Icon={LuText} />
+      </div>
+      <div className="flex">
+        <SubmitButton />
+      </div>
+    </div>
+  );
 };
